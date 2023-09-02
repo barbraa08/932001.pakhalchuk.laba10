2254306868e717bebfe0f1d3f7122c1b099c074c
barbraa08 <101864610+barbraa08@users.noreply.github.com>
2023-09-02 17:23:32 +0700

    LAB10

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtGame = new System.Windows.Forms.DataGridView();
+            this.btResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtGame
+            // 
+            this.dtGame.AllowUserToAddRows = false;
+            this.dtGame.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtGame.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dtGame.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGame.Location = new System.Drawing.Point(231, 23);
+            this.dtGame.Name = "dtGame";
+            this.dtGame.ReadOnly = true;
+            this.dtGame.RowHeadersWidth = 62;
+            this.dtGame.RowTemplate.Height = 28;
+            this.dtGame.Size = new System.Drawing.Size(685, 394);
+            this.dtGame.TabIndex = 0;
+            // 
+            // btResult
+            // 
+            this.btResult.Location = new System.Drawing.Point(36, 164);
+            this.btResult.Name = "btResult";
+            this.btResult.Size = new System.Drawing.Size(158, 111);
+            this.btResult.TabIndex = 1;
+            this.btResult.Text = "Спрогнозировать";
+            this.btResult.UseVisualStyleBackColor = true;
+            this.btResult.Click += new System.EventHandler(this.btResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 441);
+            this.Controls.Add(this.btResult);
+            this.Controls.Add(this.dtGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGame)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtGame;
+        private System.Windows.Forms.Button btResult;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Comparer : IComparer<Team>
+        {
+            public int Compare(Team x, Team y)
+            {
+                return x.points.CompareTo(y.points);
+            }
+        }
+
+        public class Team
+        {
+            public string t;
+            public double l;
+            public int points;
+
+            public Team()
+            {
+                t = "";
+                l = 0;
+                points = 0;
+            }
+        }
+
+        int round = 0;
+        Team[] teams = new Team[8];
+        List<Team> summary = new List<Team>();
+        Team team = new Team(), team1 = new Team();
+        private void btResult_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            dtGame.Rows.Clear();
+            summary.Clear();
+
+
+            var restT = new List<Team>(teams.Skip(1));
+            int teamAm = 8;
+
+            if (restT[round % restT.Count]?.Equals(default) == false)
+            {
+                int m = 0;
+                double S = 0;
+                double a = rand.NextDouble();
+                S += Math.Log(a);
+                while (S >= -teams[0].l)
+                {
+                    m++;
+                    a = rand.NextDouble();
+                    S += Math.Log(a);
+                }
+                teams[0].points += m;
+                summary.Add(teams[0]);
+
+                m = 0;
+                S = 0;
+                a = rand.NextDouble();
+                S += Math.Log(a);
+                while (S >= -restT[round % restT.Count].l)
+                {
+                    m++;
+                    a = rand.NextDouble();
+                    S += Math.Log(a);
+                }
+                restT[round % restT.Count].points += m;
+                summary.Add(restT[round % restT.Count]);
+            }
+
+            for (var index = 1; index < teamAm / 2; index++)
+            {
+                var first = restT[(round + index) % restT.Count];
+                var second = restT[(round + restT.Count - index) % restT.Count];
+                if (first?.Equals(default) == false && second?.Equals(default) == false)
+                {
+                    int m = 0;
+                    double S = 0;
+                    double a = rand.NextDouble();
+                    S += Math.Log(a);
+                    while (S >= -first.l)
+                    {
+                        m++;
+                        a = rand.NextDouble();
+                        S += Math.Log(a);
+                    }
+                    first.points += m;
+                    summary.Add(first);
+
+                    m = 0;
+                    S = 0;
+                    a = rand.NextDouble();
+                    S += Math.Log(a);
+                    while (S >= -second.l)
+                    {
+                        m++;
+                        a = rand.NextDouble();
+                        S += Math.Log(a);
+                    }
+                    second.points += m;
+                    summary.Add(second);
+                }
+            }
+
+            Comparer comp = new Comparer();
+            summary.Sort(comp);
+            summary.Reverse();
+            dtGame.ColumnCount = 3;
+            dtGame.Columns[0].HeaderText = "POS";
+            dtGame.Columns[1].HeaderText = "Team";
+            dtGame.Columns[2].HeaderText = "Won";
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (round == 0) dtGame.Rows.Add();
+                dtGame.Rows[i].Cells[0].Value = i + 1;
+                dtGame.Rows[i].Cells[1].Value = summary[i].t;
+                dtGame.Rows[i].Cells[2].Value = summary[i].points;
+            }
+            for (int i = 0; i < 8; i++)
+                summary[i].points = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++) { teams[i] = new Team(); }
+
+            teams[0].t = "Chelsea"; teams[0].l = 1.9;
+            teams[1].t = "Liverpool"; teams[1].l = 1.5;
+            teams[2].t = "Arsenal"; teams[2].l = 1.4;
+            teams[3].t = "Man City"; teams[3].l = 1.1;
+            teams[4].t = "Tottenham"; teams[4].l = 0.9;
+            teams[5].t = "Man Utd"; teams[5].l = 0.8;
+            teams[6].t = "West Brom"; teams[6].l = 0.7;
+            teams[7].t = "Everton"; teams[7].l = 0.5;
+        }
+    }
+}
